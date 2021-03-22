22d9c9d600965db8e3ddf9fe03a7a9bdf54e55b9
stephen fusco <smf37@njit.edu>
2021-03-22 15:42:00 -0400

    Task: Added Calc to test all modes

---
+++ Calculator/descriptive_stat_calc.py
@@ -0,0 +1,14 @@
+from Calculator.calculator import Calculator
+from Calculator.functions.descriptive_stat_functions import DescriptiveStatFunctions
+
+class DescriptiveStatCalc(Calculator):
+
+    def __init__(self):
+        Calculator.__init__(self)
+
+    def mean(self, input_list: list) -> float:
+        self.result.append(DescriptiveStatFunctions.mean(input_list))
+        return self.result[-1]
+
+
+
