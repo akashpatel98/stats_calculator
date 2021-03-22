1a7113a55504b1ccd4d729d3b38e24426179ac3f
stephen fusco <smf37@njit.edu>
2021-03-22 17:16:17 -0400

    Task: Test descriptive_stat_calc

--- Tests/test_Descriptive_Stat.py
+++ Tests/test_Descriptive_Stat.py
@@ -2,6 +2,7 @@ import unittest
 import random
 import statistics
 from Calculator.descriptive_stat_calc import DescriptiveStatCalc
+from scipy.stats import zscore as scipy_zscore
 
 
 class TestCases(unittest.TestCase):
@@ -46,6 +47,28 @@ class TestCases(unittest.TestCase):
         expectedVal = statistics.pvariance(rList)
         actualVal = self.calculator.variance(rList)
         self.assertAlmostEqual(expectedVal, actualVal)
+
+    def test_Standard_Deviation(self):
+        rList = []
+        for i in range(0, 100000):
+            rNumber = random.randint(0, 1000)
+            rList.append(rNumber)
+        expectedVal = statistics.pstdev(rList)
+        actualVal = self.calculator.standard_deviation(rList)
+        self.assertAlmostEqual(expectedVal, actualVal)
+
+    def test_zScore(self):
+        rList = []
+        for i in range(0, 100000):
+            rNumber = random.randint(0, 1000)
+            rList.append(rNumber)
+        expectedVal = scipy_zscore(rList)
+        actualVal = self.calculator.zScore(rList)
+        self.assertEqual(expectedVal.size, len(actualVal))
+        for i in range(0, expectedVal.size):
+            self.assertAlmostEqual(expectedVal[i], actualVal[i])
+
+
 
 
 
