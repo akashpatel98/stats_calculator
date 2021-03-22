3a923db7792d5b9666295a8995775b3897d2e9b9
stephen fusco <smf37@njit.edu>
2021-03-22 16:58:29 -0400

    Feature: added def for all functions to test

--- Calculator/descriptive_stat_calc.py
+++ Calculator/descriptive_stat_calc.py
@@ -14,12 +14,20 @@ class DescriptiveStatCalc(Calculator):
         self.result.append(DescriptiveStatFunctions.median(input_list))
         return self.result[-1]
 
-    def mode(self, input_list: list) -> float:
+    def mode(self, input_list: list) -> list:
         self.result.append(DescriptiveStatFunctions.mode(input_list))
         return self.result[-1]
 
     def variance(self, input_list: list) -> float:
         self.result.append(DescriptiveStatFunctions.variance(input_list))
+        return self.result[-1]
+
+    def standard_deviation(self, input_list: list) -> float:
+        self.result.append(DescriptiveStatFunctions.standard_deviation(input_list))
+        return self.result[-1]
+
+    def zScore(self, input_list: list) -> list:
+        self.result.append(DescriptiveStatFunctions.zScore(input_list))
         return self.result[-1]
 
 
