1e1c357a97f9858f79b8113914e1767e6b603eab
stephen fusco <smf37@njit.edu>
2021-03-22 02:52:07 -0400

    Feature: Used functions.py to call the static methods

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,5 +1,6 @@
-from Operations.Addition import addition
-from Operations.Subtraction import subtraction
+from Calculator.functions.functions import Functions
+#from Operations.Addition import addition
+#from Operations.Subtraction import subtraction
 
 
 class Calculator:
@@ -9,9 +10,25 @@ class Calculator:
         pass
 
     def add(self, a, b):
-        self.result = addition(a, b)
+        self.result = Functions.Addition(a, b)
         return self.result
 
     def subtract(self, a, b):
-        self.result = subtraction(a, b)
+        self.result = Functions.Subtraction(a, b)
+        return self.result
+
+    def multiply(self, a, b):
+        self.result = Functions.Multiplication(a, b)
+        return self.result
+
+    def division(self, a, b):
+        self.result = round(Functions.Division(a, b), 9)
+        return self.result
+
+    def square(self, a):
+        self.result = Functions.Square(a)
+        return self.result
+
+    def squareRoot(self, a):
+        self.result = Functions.Square_Root(a)
         return self.result
