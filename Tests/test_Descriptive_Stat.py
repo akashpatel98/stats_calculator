40323da26dbd32a6892ecc85a768212b5b5f2a89
stephen fusco <smf37@njit.edu>
2021-03-22 16:02:15 -0400

    Task: Test Descriptive stat calc

--- Tests/test_Descriptive_Stat.py
+++ Tests/test_Descriptive_Stat.py
@@ -1,6 +1,25 @@
 import unittest
+import random
+import statistics
+from Calculator.descriptive_stat_calc import DescriptiveStatCalc
+
 
 class TestCases(unittest.TestCase):
     def setUp(self) -> None:
+        self.calculator = DescriptiveStatCalc()
+
+    def testInstantiateCalculator(self):
+        self.assertIsInstance(self.calculator, DescriptiveStatCalc)
+
+    def testMean(self):
+        rList = []
+        for i in range(0, 100000):
+            rNumber = random.randint(0, 1000)
+            rList.append(rNumber)
+        expectedVal = statistics.mean(rList)
+        actualVal = self.calculator.mean(rList)
+        self.assertEqual(expectedVal, actualVal)
+
+
 
 
