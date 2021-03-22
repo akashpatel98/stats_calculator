6b8fb5545337abb044f83978383ac3b31cc0f2b6
stephen fusco <smf37@njit.edu>
2021-03-22 16:08:26 -0400

    Feature: Added mean and median static methods

--- Calculator/functions/descriptive_stat_functions.py
+++ Calculator/functions/descriptive_stat_functions.py
@@ -7,3 +7,16 @@ class DescriptiveStatFunctions:
         for number in numbers:
             sum = Functions.Addition(sum, number)
         return Functions.Division(sum, len(numbers))
+
+    @staticmethod
+    def median(numbers: list) -> float:
+        numbers = sorted(numbers)
+        if len(numbers) % 2 == 1:
+            midIndex = int(Functions.Division(len(numbers) - 1, 2))
+            median = numbers[midIndex]
+        else:
+            leftMidIndex = int(Functions.Subtraction(Functions.Division(len(numbers) - 1, 2), 0.5))
+            rightMidIndex = int(Functions.Addition(Functions.Division(len(numbers) - 1, 2), 0.5))
+            median = Functions.Division(Functions.Addition(numbers[leftMidIndex], numbers[rightMidIndex]), 2)
+        return float(median)
+
